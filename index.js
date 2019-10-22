8bc02e5ce92414ca3662b1fc946cb21fb7d6a21c
Martin Balke <Mrtnbalke@gmail.com>
2019-10-22 08:27:11 -0700

    jsdoc comments

--- index.js
+++ index.js
@@ -4,25 +4,38 @@ const Teams = require('./models/teams.js');
 const People = require('./models/people.js');
 const Validator = require('./lib/validator.js');
 
-
+//Create a team and people data model based off of command line args
 let people = new People(process.argv.slice(2)[0]);
 let teams = new Teams(process.argv.slice(3)[0]);
 
+/**
+ * Load data takes runs at the start of program to ensure both teams
+ * and people's data are populated
+ */
 async function loadData() {
   await people.load();
   await teams.load();
 }
 
+/**
+ * Create person takes in a person object, validates it, gives it a team if none is present
+ * and inserts it in to the database
+ * @param {object} person - The person object you would like to add to the database
+ * @returns {object} - The newly created person with their team assignment
+ */
 async function createPerson(person) {
-
   let team = await findTeam(person.team);
   if (!team.id) {
     team = await createTeam(person.team);
   }
   return await people.create({ ...person, team: team.id });
 }
+
+/**
+ * Create team takes in a string and creates a team based off of that name
+ * @param {string} teamName 
+ */
 async function createTeam(teamName) {
-  
   if(Validator.isString(teamName)){
     return await teams.create({ name: teamName });
   }
@@ -30,15 +43,24 @@ async function createTeam(teamName) {
 }
 
 
-
+/**
+ * Find team checks if the value provided is of type UUID or string  it then calls teams.read()
+ * with the corrosponding key:value pair
+ * @param {string} val - The name or ID of a team you want to find
+ * @returns {object}  - The team that was found or the boolean false
+ */
 async function findTeam(val) {
-
   let result = false;
   if (Validator.isString(val)) result = await teams.read('name', val);
   else if (Validator.isUUID(val)) result = await teams.read('id', val);
   return result;
 }
 
+/**
+ * Read person searches the people database for a person based off of id
+ * @param {UUID} id - The UUID you wish to search by
+ * @returns {object}  - The person that was found
+ */
 async function readPerson(id) {
   let result = {};
   if(Validator.isUUID(id)) result = await people.read('id', id);
@@ -46,6 +68,12 @@ async function readPerson(id) {
   return result;
 }
 
+/**
+ * Update person is called whenever someone would like to change teams. If that person changes teams and their previous team is empty
+ * that team is then deleted from the database
+ * @param {UUID} id - UUID to search by
+ * @param {string} newTeam - Name of the team they wish to switch to
+ */
 async function updatePerson(id, newTeam) {
   let team = await findTeam(newTeam);
   let person = await readPerson(id);
@@ -54,30 +82,46 @@ async function updatePerson(id, newTeam) {
   return await people.update(id, {...person, team: team.id});
 }
 
+/**
+ * Deletes a person based on their UUID. Also calls delete team in case they were the last person on the team
+ * @param {*} id - UUID to search by
+ * @returns {object} - The deleted person
+ */
 async function deletePerson(id) {
   let person = await readPerson(id);
   await deleteTeam(person.team);
   return people.delete(id);
 }
 
+/**
+ * Checks the database for a team and if it has less than 2 members deletes it
+ * @param {UUID} team - UUID to search by
+ */
 async function deleteTeam(team){
   let oldTeam = people.database.filter((player) => player.team === team);
   if (oldTeam.length < 2) teams.delete(team);
 }
 
+/**
+ * Print teams console logs the current structure of the teams
+ */
 async function printTeams() {
   //credit to Fun Fun Function for the reduce portion of this function
   let teamMembers =  people.database
+  //Map out the people and use their name to give them a full name property and a team UUID
     .map( (person) => {
       let fullName = `${person.firstName} ${person.lastName}`;
       if(!person.team) person.team = 'No team';
       return [person.team, fullName];}) 
+    //Reduce the teams down in to their respective team ID's
     .reduce( (teams, players) => {
       teams[players[0]] = teams[players[0]] || [];
       teams[players[0]].push(players[1]);
       return teams;
     }, {});
   let teamslist = [];
+  //Compare the team ID's on the team members array to the teams database and then push them on to a teams list array
+  //with a team name instead of a team ID
   Object.entries(teamMembers).forEach((entry) =>{
     teams.database.forEach( (team) => {
       if(entry[0] === team.id) teamslist.push([team.name, entry[1]]);
@@ -86,6 +130,9 @@ async function printTeams() {
   console.table(teamslist);
 }
 
+/**
+ * Run operations completes all the neccessary steps for this lab as per the readme
+ */
 async function runOperations() {
   try {
     await loadData();
--- models/model.js
+++ models/model.js
@@ -8,6 +8,11 @@ const validator = require('../lib/validator.js');
 const readFile = util.promisify(fs.readFile);
 const writeFile = util.promisify(fs.writeFile);
 
+/**
+ * class Model defines how data in the application is handled
+ * @param {object} schema - this is a set of rules inside of an object to define the data
+ * @param {string} file - a string which is the file location on hard drive. Provided via command line
+ */
 class Model {
   constructor(schema, file) {
     this.schema = schema;
@@ -15,7 +20,9 @@ class Model {
     this.database = [];
   }
 
-  // Initialize the database
+  /**
+   * Load is a method on the class that loads the contents from this.file
+   */
   async load() {
     // read the file asynchronously and save the results in
     // contents
@@ -26,19 +33,31 @@ class Model {
     return this.database;
   }
 
-
+  /**
+   * Create is used to insert an object in to the database. It is given a UUID and sanitized.
+   * @param {object} item - Item you wish to insert in to the database.
+   * @returns {Promise<object>} - The created item.
+   */
   async create(item) {
-
+    //Adding a UUID and checking the item against the schema before adding it.
     let record = { id: uuid(), ...item };
     let isValid = this.sanitize(record);
+    //If the item matches the schema type push it on to this.database and save it
     if (isValid) {
       this.database.push(record);
       await writeFile(this.file, JSON.stringify(this.database));
+      //return the item that was added
       return record;
     }
+    //If the item does not pass sanitizer return
     return 'Invalid schema';
   }
-
+  /**
+   * Read searches the database for a match of both key and val and returns the found item
+   * @param {string} key - The key that you should search for IE 'id' or 'firstName'
+   * @param {string} val  - The value in the key value pair
+   * @returns {Promise<object>} -  The item that was found
+   */
   async read(key, val) {
 
     let found = {};
@@ -50,6 +69,12 @@ class Model {
     return found;
   }
 
+  /**
+   * Update searches the database by ID then if there is a match updates that item
+   * @param {string} id - ID to search by
+   * @param {object} item - Updated object properties
+   * @returns {string} - Notifies you of a successful update
+   */
   async update(id, item) {
     console.log(item);
     if(this.sanitize(item)){
@@ -64,7 +89,10 @@ class Model {
     console.error('Data provided does not match schema');
   }
 
-  // CRUD: delete
+  /**
+   * Delete searches the database for the provided id and then removes that item
+   * @param {string} id - The id of the item you wish to search for
+   */
   async delete(id) {
     let matchIndex = this.database.findIndex( (value) => value.id === id);
     if(matchIndex > -1){
@@ -72,10 +100,13 @@ class Model {
       await writeFile(this.file, JSON.stringify(this.database));
       return 'Deleted Successfully';
     }
-    console.error('Id does not match any instance in the database');
   }
 
-  // Validation
+  /**
+   * Sanitize uses the validator library to check all items inserted against the this.schema
+   * @param {object} item - Object you want to validate
+   * @returns {boolean}
+   */
   sanitize(item) {
     return validator.isValid(this.schema, item);
   }
